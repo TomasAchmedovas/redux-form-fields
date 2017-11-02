f03b1009d5050a99da15a509439208a4ac09461c
Tomas Achmedovas <achmedovas.tomas@gmail.com>
2017-11-02 18:56:53 +0200

    Add inputSize

--- src/SubmitField.js
+++ src/SubmitField.js
@@ -6,25 +6,25 @@ const SubmitField = ({
   submitting,
   submitText,
   ...field
-}: Object) => (
-  <div className="form-group">
-    <div
-      className={
-        field.vertical
-          ? ''
-          : field.block ? 'col-sm-12' : 'col-sm-9 col-sm-offset-3'
-      }
-    >
-      <button
-        className={`btn btn-primary${field.block ? ' btn-block' : ''}`}
-        disabled={disabled}
-        name="submit"
-        type="submit"
-      >
-        {submitting ? 'Please wait...' : submitText || 'Submit'}
-      </button>
+}: Object) => {
+  const inputSize = field.vertical
+    ? ''
+    : field.inputSize || 'col-sm-9 col-sm-offset-3'
+
+  return (
+    <div className="form-group">
+      <div className={inputSize}>
+        <button
+          className={`btn btn-primary${field.block ? ' btn-block' : ''}`}
+          disabled={disabled}
+          name="submit"
+          type="submit"
+        >
+          {submitting ? 'Please wait...' : submitText || 'Submit'}
+        </button>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default SubmitField
